119ff28fb4a6ff15a6c6ac49bc10b8132d2e92ce
Emuohwo <warriisaac@outlook.com>
2024-02-22 06:33:09 +0100

    Add logout script and enhance code

--- ConsoleApp1/Class1Demo.cs
+++ ConsoleApp1/Class1Demo.cs
@@ -29,7 +29,7 @@ namespace ConsoleApp1
         [TearDown]
         public void TearDown()
         {
-           //  _webDriver.Quit();
+             _webDriver.Quit();
         }
 
         [Test]
@@ -37,9 +37,8 @@ namespace ConsoleApp1
         {
             // 1. Navigate to login page
             _webDriver.Navigate().GoToUrl("https://www.saucedemo.com/");
-             // Assert.True(_webDriver.Title.Contains("Google"));
 
-            // 2. Enter valid credentials to log in.l
+            // 2. Enter valid credentials to log in.
             _webDriver.FindElement(By.Id("user-name")).SendKeys("standard_user");
             _webDriver.FindElement(By.Id("password")).SendKeys("secret_sauce");
             _webDriver.FindElement(By.Id("login-button")).Click();
@@ -47,62 +46,98 @@ namespace ConsoleApp1
             //3. Verify that the login is successful and the user is redirected to the products page.
             String pageURL = "https://www.saucedemo.com/inventory.html";
              Assert.That(pageURL, Is.EqualTo(_webDriver.Url));
-            // Console.WriteLine(pageURL);
+
+            string productPageUrl = _webDriver.Url;
+
+            if (productPageUrl.Contains("inventory"))
+            {
+                Console.WriteLine("Login was successful.");
+            }
+            else
+            {
+                Console.WriteLine("An error occured while loading the page. Please try again later");
+            }
+
 
             // 4. Select a T-shirt by clicking on its image or name.
             _webDriver.FindElement(By.PartialLinkText("T-Shirt")).Click();
-
-            // 5. Verify that the T-shirt details page is displayed.
-           String productTitle = _webDriver.FindElement(By.XPath("//div[@class='inventory_details_name large_size']")).Text;
-            Assert.That(productTitle, Does.Contain("T-Shirt"));
             
+               // 5. Verify that the T-shirt details page is displayed.
+               String productTitle = _webDriver.FindElement(By.XPath("//div[@class='inventory_details_name large_size']")).Text;
+                Assert.That(productTitle, Does.Contain("T-Shirt"));
 
-            // 6. Click the "Add to Cart" button.
-            _webDriver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt")).Click();
-
-            // 7. Verify that the T-shirt is added to the cart successfully.
-           String cartCount = _webDriver.FindElement(By.XPath("//span[@class='shopping_cart_badge']")).Text;
-            int n = Int32.Parse(cartCount);
-            if (n == 1)
-            {
-                Console.WriteLine("T-shirt is added to the cart successfully");
-            } else
-            {
-                Console.WriteLine("Failed to add product to cart");
-            }
-
-            // 8. Navigate to the cart by clicking the cart icon or accessing the cart page directly.
-            _webDriver.FindElement(By.XPath("//a[@class='shopping_cart_link']")).Click();
-
-
-
-            // 9. Verify that the cart page is displayed.
-            String cartPageURL = "https://www.saucedemo.com/cart.html";
-            Assert.That(cartPageURL, Is.EqualTo(_webDriver.Url));
             
+                // 6. Click the "Add to Cart" button.
+                _webDriver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt")).Click();
 
-            // 10. 
+                // 7. Verify that the T-shirt is added to the cart successfully.
+                String cartCount = _webDriver.FindElement(By.XPath("//span[@class='shopping_cart_badge']")).Text;
+                int n = Int32.Parse(cartCount);
+                if (n == 1)
+                {
+                    Console.WriteLine("T-shirt is added to the cart successfully");
+                } else
+                {
+                    Console.WriteLine("Failed to add product to cart");
+                }
 
-            // 11. Click the "Checkout" button.
-            _webDriver.FindElement(By.Id("checkout")).Click();
-            //*[@id="header_container"]/div[2]/span
-            //span[@class='title']
+                // 8. Navigate to the cart by clicking the cart icon or accessing the cart page directly.
+                _webDriver.FindElement(By.XPath("//a[@class='shopping_cart_link']")).Click();
 
 
-            // 12. Verify that the checkout information page is displayed
-            String pageTitle = _webDriver.FindElement(By.XPath("//span[@class='title']")).Text;
-            String checkoutPageTitle = "Checkout: Your Information";
-            Assert.That(pageTitle, Is.EqualTo(checkoutPageTitle));
+
+                // 9. Verify that the cart page is displayed.
+                String cartPageURL = _webDriver.Url;
+                if (cartPageURL.Contains("cart"))
+                {
+                    Console.WriteLine("Cart loaded successfully");
+                }
+                else
+                {
+                    Console.WriteLine("An error occured while loading the cart. Please try again later");
+                }
+
+
+            // 10. Review the items in the cart and ensure that the T-shirt is listed with the correct details (name, price,
+
+            String qty = _webDriver.FindElement(By.XPath("//div[@class='cart_quantity']")).Text;
+                int cartQty = Int32.Parse(qty);
+                if (cartQty == 1)
+                {
+                    Console.WriteLine("Quantity of selected T-Shirt is 1");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to get Quantity of T-Shirt");
+                }
+
+                // 11. Click the "Checkout" button.
+                _webDriver.FindElement(By.Id("checkout")).Click();
+
+
+                // 12. Verify that the checkout information page is displayed
+                String pageTitle = _webDriver.FindElement(By.XPath("//span[@class='title']")).Text;
+                String checkoutPageTitle = "Checkout: Your Information";
+                Assert.That(pageTitle, Is.EqualTo(checkoutPageTitle));
+                string checkoutPageURL = _webDriver.Url;
+                if (checkoutPageURL.Contains("checkout"))
+                {
+                    Console.WriteLine("Checkout loaded successfully");
+                }
+                else
+                {
+                    Console.WriteLine("An error occured while loading the checkout. Please try again later");
+                }
 
 
             // 13.Enter the required checkout information (e.g., name, shipping address, payment details).
             _webDriver.FindElement(By.Id("first-name")).SendKeys("John");
-            _webDriver.FindElement(By.Id("last-name")).SendKeys("Doe");
-            _webDriver.FindElement(By.Id("postal-code")).SendKeys("100023");
+                _webDriver.FindElement(By.Id("last-name")).SendKeys("Doe");
+                _webDriver.FindElement(By.Id("postal-code")).SendKeys("100023");
 
 
-            // 14. Click the "Continue" button.
-            _webDriver.FindElement(By.Id("continue")).Click();
+                // 14. Click the "Continue" button.
+                _webDriver.FindElement(By.Id("continue")).Click();
 
             // 15.Verify that the order summary page is displayed, showing the T-shirt details and the total amount.
             String summaryPageURL = "https://www.saucedemo.com/checkout-step-two.html";
@@ -122,31 +157,35 @@ namespace ConsoleApp1
 
 
             // 17.Verify that the order confirmation page is displayed, indicating a successful purchase.
-            String confirmationPageURL = "https://www.saucedemo.com/checkout-complete.html";
-            if (confirmationPageURL == _webDriver.Url)
+            string confirmationPageURL = _webDriver.Url;
+            if (confirmationPageURL.Contains("checkout-complete"))
             {
-                Console.WriteLine("order confirmation page is displayed");
+                Console.WriteLine("Thank you for your order!");
             }
             else
             {
-                Console.WriteLine("Failed to load order confirmation page");
+                Console.WriteLine("An error occured while placing your order. Please try again later");
             }
-            IWebElement successMsg = _webDriver.FindElement(By.TagName("h2"));
-            Assert.Pass(("Thank you for your order!"));
-
             
-
 
 
             // 18. Logout from the application.
             _webDriver.FindElement(By.Id("react-burger-menu-btn")).Click();
-           IWebElement menu = _webDriver.FindElement(By.Id("bm-menu- wrap"));
-            _webDriver.SwitchTo().ActiveElement();
-            _webDriver.FindElement(By.Id("logout_sidebar_link")).Click();
-
-            // 19. 
+            IWebElement logoutLink = _webDriver.FindElement(By.Id("logout_sidebar_link"));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)_webDriver;
+            js.ExecuteScript("arguments[0].click();", logoutLink);
 
 
+            // 19. Verify that the user is successfully logged out and redirected to the login page.
+            string loginPageURL = _webDriver.Url;
+            if (loginPageURL == "https://www.saucedemo.com/")
+            {
+                Console.WriteLine("Logout was successful");
+            }
+            else
+            {
+                Console.WriteLine("An error occured while logging out. Please try again later");
+            }
 
         }
 
